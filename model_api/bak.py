d3a609b132bca4995fdfaeff9d63faea8421d605
wangyunyi <29663732@qq.com>
2018-10-26 19:35:34 +0800

    1

---
+++ model_api/bak.py
@@ -0,0 +1,8 @@
+#jupyter server配置
+#https://blog.csdn.net/simple_the_best/article/details/77005400
+
+# flask远程服务连接
+# https://blog.csdn.net/dreamer23/article/details/82882776
+#
+# pycharm连接github
+# https://www.cnblogs.com/feixuelove1009/p/5955332.html
